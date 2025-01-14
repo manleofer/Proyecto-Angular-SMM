220d0979d817ac42ad00237e612dbd706f181778
manleofer <mleofer304@iesalmudeyne.es>
2025-01-14 21:03:19 +0100

    Exporto libreria commondModule, creo una variable en app.components.ts y una funcion, y la agrego en app.component.html, todo con la directiva ngIf.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ProyectoAngular';
+
+  mostrarMenu: boolean = false;
+  fMostrarMenu(){
+    this.mostrarMenu = !this.mostrarMenu;
+  }
+
 }
 
 
