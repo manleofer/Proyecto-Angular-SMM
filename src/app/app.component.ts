b8e8485f78803dd65214bc5a6ec83f246b890956
mcaruisan <mcarmen.rs79@gmail.com>
2025-01-15 09:38:33 +0100

    Se añade directiva ngSwitch en html y ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ProyectoAngular';
+
+  //Crear una variable y definir el color inicial del texto del párrafo
+  colorTexto : string = 'black';
+
+  //Crear un método para cambiar el color del texto al pulsar los botones
+  cambiarColorTexto(nuevoColor: string) {
+    this.colorTexto = nuevoColor;
+  }
 }
 
 
