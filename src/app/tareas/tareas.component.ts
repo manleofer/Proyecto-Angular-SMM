e2cb2112c9a383b50d6d7198762585e841071c23
sanmorher <smorher483@iesalmudeyne.es>
2025-01-25 18:52:36 +0100

    Reutilizo componente 'tareas' en el apartado Directivas

--- src/app/tareas/tareas.component.ts
+++ src/app/tareas/tareas.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tareas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tareas.component.html',
   styleUrl: './tareas.component.css'
 })
