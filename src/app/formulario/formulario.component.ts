d9c072bd8fb16170f9af4117caebc13500802ff6
mcaruisan <mcarmen.rs79@gmail.com>
2025-02-02 17:53:51 +0100

    Se mueve la definición de formulario desde app.component hasta formulario.component

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-formulario',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, FooterComponent],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
@@ -16,23 +17,5 @@ export class FormularioComponent {
     apellido: "",
     pwd: ""
   };
-
-  //Variable usada para validar contraseña inicializada en false
-  esValida = false;
-
-  /*   //Variable que valida el formulario
-    formularioValido = false;
-    formularioEnviado = false; */
-
-  //Método para validar la contraseña
-//Tiene que tener longitud 8 y estar formada por letras y dígitos
-  validarPwd(): void {
-    const pwd = this.registro.pwd;
-    const longitud = pwd.length >= 8;
-    const letra = /[a-zA-Z]/.test(pwd);
-    const digito = /\d/.test(pwd);
-
-    this.esValida = longitud && letra && digito;
-  }
 
 }
