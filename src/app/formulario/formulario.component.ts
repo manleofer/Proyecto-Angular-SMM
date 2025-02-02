0ddc1fa85d349856f50a581d24434f0a9a096413
mcaruisan <mcarmen.rs79@gmail.com>
2025-02-02 11:28:08 +0100

    se añaden fieldset Datos Personales y fieldset Cursos

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,13 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+
+  //Objeto registro en JSON
+  registro = {
+    nombre: "",
+    apellido: "",
+    pwd: ""
+  };
+
+/*   //Variable que valida el formulario
+  formularioValido = false;
+  formularioEnviado = false; */
+
+//Método para validar la contraseña
+//Tiene que tener longitud 8 y estar formada por letras y dígitos
+validarPwd(): boolean{
+  const pwd = this.registro.pwd;
+  const longitud = pwd.length == 8;
+  const letra = /[a-zA-Z]/.test(pwd);
+  const digito = /\d/.test(pwd);
+
+  return longitud && letra && digito;
+}
+
 
 }
