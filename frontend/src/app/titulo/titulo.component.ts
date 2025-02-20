58967cb1a06cb5df40c71d223478a0de42d96413
sanmorher <smorher483@iesalmudeyne.es>
2025-02-20 10:47:44 +0100

    Añado componente Titulo (home - vista principal)

---
+++ frontend/src/app/titulo/titulo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-titulo',
+  imports: [FooterComponent],
+  templateUrl: './titulo.component.html',
+  styleUrl: './titulo.component.css'
+})
+
+export class TituloComponent {
+
+}
