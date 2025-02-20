0144d100c1aeb5c42bfcb8d5c544c1d4b058d38b
sanmorher <smorher483@iesalmudeyne.es>
2025-02-20 20:41:35 +0100

    Corrección erratas en erutamiento

--- frontend/src/app/formulario/formulario.component.ts
+++ frontend/src/app/formulario/formulario.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-formulario',
-  imports: [CommonModule, FormsModule, FooterComponent],
+  imports: [CommonModule, FormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
--- frontend/src/app/titulo/titulo.component.ts
+++ frontend/src/app/titulo/titulo.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-titulo',
-  imports: [FooterComponent],
+  imports: [],
   templateUrl: './titulo.component.html',
   styleUrl: './titulo.component.css'
 })
