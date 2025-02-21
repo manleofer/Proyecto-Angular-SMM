a38c6c6a4c6f773281d80617711631a3f93f9488
manleofer <mleofer304@iesalmudeyne.es>
2025-02-21 17:38:15 +0100

    Cambio algunos conflictos del merge

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { rutas } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(rutas)  //Se configuran las rutas
+    provideRouter(rutas),  //Se configuran las rutas
+    provideHttpClient()
   ]};
