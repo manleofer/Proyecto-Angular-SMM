679133a6c005d9a17e498c58e9c58f93d115b31b
sanmorher <smorher483@iesalmudeyne.es>
2025-02-21 22:09:18 +0100

    DELETE Alumno codificado en frontend

---
+++ frontend/src/app/formularios-delete/formularios-delete.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { AlumnoService } from '../services/alumno.service';
+import { firstValueFrom } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formularios-delete',
+  imports: [FormsModule],
+  templateUrl: './formularios-delete.component.html',
+  styleUrl: './formularios-delete.component.css'
+})
+
+export class FormulariosDeleteComponent implements OnInit {
+  //Creo el array donde almacenar los alumnos que recibo del backend
+  alumnos: any[] = [];
+
+  idSeleccionado: number = 0;
+
+  //Inyección de servicios
+  constructor(private alumnoService: AlumnoService) { }
+
+  //Métodos que se ejecutan al cargar el componente
+  ngOnInit(): void {
+    this.getAlumnos();
+  }
+
+  //Método para obtener todos los alumnos desde el backend
+  async getAlumnos(): Promise<void> {
+    try {
+      const response = await firstValueFrom(this.alumnoService.getAlumnos());
+      this.alumnos = response;  
+    } catch (error) {
+      alert('Error al cargar los alumnos');
+    }
+  }
+
+
+  // Método para eliminar el alumno seleccionado
+  async eliminarAlumno(): Promise<void> {
+    if (this.idSeleccionado === null) {
+      alert('Debe seleccionar un alumno');  
+      return;
+    }
+      try {
+      const response = await firstValueFrom(this.alumnoService.deleteAlumno(this.idSeleccionado));
+      alert('Alumno eliminado correctamente');
+      this.getAlumnos();
+    } catch (error) {
+      alert('Error al eliminar el alumno'); 
+    }
+  }
+}
---
+++ frontend/src/app/services/alumno.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AlumnoService {
+
+  private apiUrl = 'http://localhost:3000/api/alumnos'; // URL de la API en backend
+
+  //Se pasa al constructor una instancia de Angular para hacer solicitudes HTTP desde el frontend hacia el backend
+  constructor(private http: HttpClient) { }   
+
+
+  //Método para obtener todos los alumnos
+  getAlumnos(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+  
+
+
+  // Método para eliminar un alumno
+  deleteAlumno(id: number): Observable<any> {
+    return this.http.delete<any>(this.apiUrl + "/delete/" + id);
+  }
+
+
+}
+
+
