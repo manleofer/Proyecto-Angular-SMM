dd26af69fbc21c42f9200dc21b433538e9df3d2c
mcaruisan <mcarmen.rs79@gmail.com>
2025-02-21 13:58:17 +0100

    Se añane la lógica de back para profesor y alumno

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 
 const app= express();
 const alumnoRoutes = require("./routes/alumnoRoutes");
+const profesorRoutes = require("./routes/profesorRoutes");
+const cursoRoutes = require("./routes/cursoRoutes");
 
 app.use(cors());
 app.use(express.json());  //para recibir json en las peticiones (habilita json en las peticiones)
@@ -13,7 +15,15 @@ app.get("/", (req, res) => {
     res.send("Servidor Node.js funcionando");
 }); */
 
-app.use("/api/alumnos", alumnoRoutes);  //carga las rutas de alumno
+//carga las rutas de alumno
+app.use("/api/alumnos", alumnoRoutes);  
+
+//carga las rutas de profesor
+app.use("/api/profesores", profesorRoutes); 
+
+//carga las rutas de curso
+app.use("/api/cursos", cursoRoutes); 
+
 
 // Iniciar el servidor
 app.listen(3000, () => {
--- backend/src/routes/profesorRoutes.js
+++ backend/src/routes/profesorRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bbdd = require("../config/bbdd");
 
-//Añadir un alumno en la bbdd
+//Añadir un profesor en la bbdd
 router.post("/insert", (req, res) => {
     console.log("Datos recibidos:", req.body); //
     const {nombre, telefono} = req.body;
@@ -11,12 +11,12 @@ router.post("/insert", (req, res) => {
         return res.status(400).json({message: "Todos los campos son obligatorios"});
     }
 
-    const sql = "INSERT INTO alumno (nombre, telefono) VALUES (?, ?)";
+    const sql = "INSERT INTO profesor (nombre, telefono) VALUES (?, ?)";
     bbdd.query(sql, [nombre, telefono], (error, result) => {
         if(error) {
-            return res.status(500).json({ error: "Error al insertar el alumno", details: error });
+            return res.status(500).json({ error: "Error al insertar el profesor", details: error });
         }
-        res.status(201).json({ message: "Alumno insertado con éxito", idAlumno: result.insertId });
+        res.status(201).json({ message: "Profesor insertado con éxito", idProfesor: result.insertId });
     });
 });
 module.exports = router;
