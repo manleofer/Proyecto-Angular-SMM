4d9deee6b8c5df6210c5567159ebe8137c6f446e
mcaruisan <mcarmen.rs79@gmail.com>
2025-02-22 16:22:09 +0100

    Se añade ruta para obtener todos los alumnos en alumnoRoutes.js

--- backend/src/routes/alumnoRoutes.js
+++ backend/src/routes/alumnoRoutes.js
@@ -18,5 +18,20 @@ router.post("/insert", (req, res) => {
         }
         res.status(201).json({ message: "Alumno insertado con éxito", idAlumno: result.insertId });
     });
+
+    
 });
+
+// Obtener listado de alumnos
+router.get("/listado", (req, res) => {
+    const sql = "SELECT * FROM alumnos";
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: "Error al obtener alumnos", err });
+        }
+        res.status(200).json(results);
+    });
+});
+
 module.exports = router;
