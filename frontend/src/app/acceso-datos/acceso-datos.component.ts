f45657b932c135394234bcd563bf45ad46ba4e3a
mcaruisan <mcarmen.rs79@gmail.com>
2025-02-22 16:59:49 +0100

    Se añade el componente para las consultas (SELECT)

--- frontend/src/app/acceso-datos/acceso-datos.component.ts
+++ frontend/src/app/acceso-datos/acceso-datos.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormulariosInsertComponent } from '../formularios-insert/formularios-insert.component';
+import { ConsultasComponent } from '../consultas/consultas.component';
 
 @Component({
   selector: 'app-acceso-datos',
-  imports: [CommonModule, FormulariosInsertComponent],
+  imports: [CommonModule, FormulariosInsertComponent, ConsultasComponent],
   templateUrl: './acceso-datos.component.html',
   styleUrl: './acceso-datos.component.css'
 })
